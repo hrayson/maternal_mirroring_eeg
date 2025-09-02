6378b34e980c0e8e4aae16051b38eb860f931899
Holly Rayson <43045920+hrayson@users.noreply.github.com>
2025-09-02 17:24:39 +0200

    initial import

---
+++ exe/add_movement_events.m
@@ -0,0 +1,29 @@
+
+function data=add_movement_events(subj_id, data, behavior_file)
+
+behavior_fid=fopen(['/data/infant_9m_face_eeg/raw/' num2str(subj_id) '/' behavior_file]);
+lines=textscan(behavior_fid,'%s','Delimiter','\n','CollectOutput',true);
+for i=3:length(lines{1})
+    cols=strsplit(lines{1}{i},',');
+    movement=cols{2};
+
+    init_onset=cols{3};
+    init_onset_min=str2num(init_onset(1:2));
+    init_onset_sec=str2num(init_onset(4:5));
+    init_onset_ms=str2num(init_onset(7:end));
+    init_onset_level=str2num(cols{4});
+
+    init_offset=cols{5};
+    init_offset_min=str2num(init_offset(1:2));
+    init_offset_sec=str2num(init_offset(4:5));
+    init_offset_ms=str2num(init_offset(7:end));    
+    init_offset_level=str2num(cols{6});
+
+    init_latency=init_onset_min*60.0+init_onset_sec+0.001*init_onset_ms;
+    init_duration=(init_offset_min*60.0+init_offset_sec+0.001*init_offset_ms)-init_latency;
+    init_magnitude=init_offset_level-init_onset_level;
+    data = pop_editeventvals(data,'append',{1 [] [] [] [] [] [] []},'changefield',{2 'type' 'artifact'},'changefield',{2 'latency' init_latency},'changefield',{2 'duration' init_duration},'changefield',{2 'actor' 'None'},'changefield',{2 'code' init_magnitude},'changefield',{2 'movement' movement});
+    %data = pop_editeventvals(data,'append',{1 'artifact' init_latency init_duration 'None' init_magnitude movement});
+    %data=eeg_addnewevents(data, {[init_latency]}, {'artifact'}, {'duration','actor','code','movement'}, {[init_duration],['None'],[init_magnitude],[movement]});
+    
+end
